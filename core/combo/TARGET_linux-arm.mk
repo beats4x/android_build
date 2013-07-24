62d68b5e7159439a5abd5f91568e7028c5f45ba8
DJLaMontagneIII <DJLaMontagneIII@gmail.com>
2013-07-23 22:29:44 -0400

    Bring back -O3

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -66,15 +66,12 @@ endif
 
 TARGET_NO_UNDEFINED_LDFLAGS := -Wl,--no-undefined
 
-TARGET_arm_CFLAGS :=    -O2 \
-                        -fgcse-after-reload \
-                        -fipa-cp-clone \
-                        -fpredictive-commoning \
-                        -fsched-spec-load \
-                        -funswitch-loops \
-                        -fvect-cost-model \
+TARGET_arm_CFLAGS :=    -O3 \
+                        -fno-tree-vectorize \
+                        -fno-inline-functions \
                         -fomit-frame-pointer \
-                        -fstrict-aliasing
+                        -fstrict-aliasing \
+                        -Wstrict-aliasing=3
 
 # Modules can choose to compile some source as thumb. As
 # non-thumb enabled targets are supported, this is treated
@@ -82,16 +79,13 @@ TARGET_arm_CFLAGS :=    -O2 \
 # compiled as ARM.
 ifeq ($(ARCH_ARM_HAVE_THUMB_SUPPORT),true)
 TARGET_thumb_CFLAGS :=  -mthumb \
-                        -Os \
-                        -fgcse-after-reload \
-                        -fipa-cp-clone \
-                        -fpredictive-commoning \
-                        -fsched-spec-load \
-                        -funswitch-loops \
-                        -fvect-cost-model \
+                        -O3 \
+                        -fno-tree-vectorize \
+                        -fno-inline-functions \
+                        -fno-unswitch-loops \
                         -fomit-frame-pointer \
                         -fstrict-aliasing \
-                        -Wstrict-aliasing=2
+                        -Wstrict-aliasing=3
 else
 TARGET_thumb_CFLAGS := $(TARGET_arm_CFLAGS)
 endif
@@ -176,20 +170,20 @@ TARGET_GLOBAL_CPPFLAGS += -fvisibility-inlines-hidden
 # More flags/options can be added here
 ifndef TARGET_EXTRA_CFLAGS
   TARGET_RELEASE_CFLAGS := \
-        -DNDEBUG \
-        -g \
-        -Wstrict-aliasing=2 \
-        -fgcse-after-reload \
-        -frerun-cse-after-loop \
-        -frename-registers
+			  -DNDEBUG \
+			  -g \
+			  -Wstrict-aliasing=3 \
+			  -fgcse-after-reload \
+			  -frerun-cse-after-loop \
+			  -frename-registers
 else
   TARGET_RELEASE_CFLAGS += \
-        -DNDEBUG \
-        -g \
-        -Wstrict-aliasing=2 \
-        -fgcse-after-reload \
-        -frerun-cse-after-loop \
-        -frename-registers
+			  -DNDEBUG \
+			  -g \
+			  -Wstrict-aliasing=3 \
+			  -fgcse-after-reload \
+			  -frerun-cse-after-loop \
+			  -frename-registers
 endif
 
 libc_root := bionic/libc
